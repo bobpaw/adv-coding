d57fd8b4e5d03ca599a357d816111010e03c410f
Aiden Woodruff <aidenpw@hotmail.com>
2018-01-10 20:51:03 -0600

    Added blkbean game and reverse program
    -Aiden Woodruff <aiden.woodruff@gmail.com>

---
+++ blkbean.py
@@ -0,0 +1,51 @@
+##
+## Black bean game. Give launching permissions and execute or execute
+## with Python3.
+## Author: Aiden Woodruff <aiden.woodruff@gmail.com>
+##
+
+#!/usr/bin/python3
+
+import random
+import time
+
+print("Whoever takes the last bean wins")
+total_beans = int(input("How many total beans: "))
+max_beans = int(input("Max beans per turn: "))
+#turn_s = input("Would you like to go first? (Y/N): ")
+#if (turn_s == "Y" or turn_s == "y"):
+#    turn = 1
+#else:
+#    turn = 0
+if total_beans % (max_beans+1) == 0:
+    turn = 1
+else:
+    turn = 0
+
+num_beans = total_beans
+
+while (num_beans > 0):
+    take = 0
+    print(str(num_beans) + " beans left.")
+    if (turn == 0):
+        print("My turn!")
+        if (num_beans % (max_beans+1) > 0):
+            take = num_beans % (max_beans+1)
+        else:
+            take = random.randint(1,max_beans)
+        time.sleep(1)
+        print("I take " + str(take) + " beans.")
+    elif (turn == 1):
+        print("Your turn!")
+        while (not(take >= 1 and take <= max_beans)):
+            take = int(input("Take how many beans: "))
+        print("You take " + str(take) + " beans.")
+    num_beans -= take
+    if (turn ==0):
+        turn = 1
+    elif (turn == 1):
+        turn = 0
+if (turn == 0):
+    print("I lose! :(")
+elif (turn == 1):
+    print("You lose! :)")
---
+++ reverse.py
@@ -0,0 +1,10 @@
+##
+## Reverse inputted text. Run with Python 3.
+## Author: Aiden Woodruff <aiden.woodruff@gmail.com>
+##
+
+user_string = input("Gimme some characters: ")
+user_list = list(user_string)
+for i in range(0, len(user_string)):
+    print(user_list.pop(), end="")
+print()
